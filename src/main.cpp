2b7888d3c0cb41520191dcde60dd605eaa454e29
turnip-greens <isaacstarr0910@gmail.com>
2026-09-04 21:28:01 -0500

    Configured Cmake, made proper constructors, fixed bugs
    - Fixed prompt function:
        - std::cin would loop / break and try blocks don't work on cin
    - fixed int / uint comparisons
    - renamed insc, circ variables to left and right sums.
    - Fixed output typo
    - Imported cmath instead of math.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <cstdint>
-#include <math.h>
 #include <string>
+#include <cmath>
 
 struct monomial
 {
@@ -13,11 +13,7 @@ struct monomial
 		return scalar * powf(x, exponent);
 	}
 
-	void define_mono(float s, float e)
-	{
-		scalar = s;
-		exponent = e;
-	}
+	monomial(float s, float e) : scalar(s), exponent(e) {}
 };
 
 struct standard_form_3
@@ -30,26 +26,26 @@ struct standard_form_3
 		return m[0].calc(x) + m[1].calc(x) + m[2].calc(x) + constant;
 	}
 	
-	void set_func(float s3, float e3, float s2, float e2, float s1, float e1, float c)
-	{
-		m[0].define_mono(s3, e3);
-		m[1].define_mono(s2, e2);
-		m[2].define_mono(s1, e1);
-		constant = c;
-	}
-
+	standard_form_3(float s3, float s2, float s1, float c)
+		: m{ {s3, 3}, {s2, 2}, {s1, 1} }, constant(c) {}
 };
 
 void prompt(std::string s, auto& v)
 {
 	std::cout << s;
-	try 
+	
+	while (true)
 	{
-		std::cin >> v;
-	}
-	catch (...)
-	{
-		std::cout << "Invalid Value";
+		if (!(std::cin >> v))
+		{
+			std::cout << "Invalid Input; try again: ";
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+		}
+		else
+		{
+			break;
+		}
 	}
 }
 
@@ -57,15 +53,14 @@ int main()
 {
 	float s3, s2, s1, c;
 	float lower_bound, upper_bound;
-	uint32_t samples;
+	int samples;
 	
 	prompt("Enter scalar of x^3: ", s3);
 	prompt("Enter scalar of x^2: ", s2);
 	prompt("Enter scalar of x: ", s1);
 	prompt("Enter constant: ", c);
 
-	standard_form_3 function;
-	function.set_func(s3, 3, s2, 2, s1, 1, c);
+	standard_form_3 function(s3, s2, s1, c);
 
 	prompt("Enter lower bound: ", lower_bound);
 	prompt("Enter upper bound: ", upper_bound);
@@ -73,16 +68,16 @@ int main()
 
 	float step_size = (upper_bound - lower_bound) / samples;
 
-	float total_area_insc = 0.f;
-	float total_area_circ = 0.f;
+	float left_sum = 0.f;
+	float right_sum = 0.f;
 
 	for (int i = 0; i < samples; i++)
 	{
-		total_area_insc += function.calc(lower_bound + i * step_size) * step_size;
-		total_area_circ += function.calc(lower_bound + (i + 1) * step_size) * step_size;
+		left_sum += function.calc(lower_bound + i * step_size) * step_size;
+		right_sum += function.calc(lower_bound + (i + 1) * step_size) * step_size;
 	}
 
-	std::cout << "\intergral " << lower_bound << "<->" << upper_bound << " = " << (total_area_circ + total_area_insc) / 2 << "\n";
+	std::cout << "Intergral " << lower_bound << "<->" << upper_bound << " = " << (left_sum + right_sum) / 2 << "\n";
 
 	return 0;
 }
